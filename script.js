cc178153103715d89606e10af30883bd14043ece
SteveNoyes <stevenmnoyes.af@gmail.com>
2023-04-11 10:53:26 -0400

    full row font size adjust

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ $( document ).ready(function() {
 			}    
     });
 });
+
+$( document ).ready(function() {
+	$('.info-row').each(function() {
+		var $numWords = $(this).text().split(" ").length;
+		console.log($numWords);
+		if (($numWords >= 1) && ($numWords < 10)) {
+			$(this).parent().css("font-size", "30px");
+			console.log("font size 30px");
+		}
+		else if (($numWords >= 10) && ($numWords < 20)) {
+			$(this).parent().css("font-size", "25px");
+			console.log("font size 25px");
+		}
+		else if (($numWords >= 20) && ($numWords < 30)) {
+			$(this).parent().css("font-size", "20px");
+			console.log("font size 20px");
+		}
+		else if (($numWords >= 30) && ($numWords < 40)) {
+			$(this).parent().css("font-size", "16px");
+			console.log("font size 16px");
+		}
+		else {
+			$(this).parent().css("font-size", "12px");
+			console.log("default 12px");
+		}    
+	});
+});
